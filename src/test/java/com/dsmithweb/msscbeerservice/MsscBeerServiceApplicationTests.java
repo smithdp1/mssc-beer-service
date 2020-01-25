3707613a0e74b131b233eabee5ba585b69be53fe
David Smith <=>
2020-01-25 14:21:57 -0700

    Initial project add. Closes #1

---
+++ src/test/java/com/dsmithweb/msscbeerservice/MsscBeerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dsmithweb.msscbeerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsscBeerServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
